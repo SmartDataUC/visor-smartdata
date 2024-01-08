a981ea2da12ed2beb9b0a2609c8c9ad34b4ace65
Joshua Kunst <jbkunst@gmail.com>
2024-01-08 15:23:08 -0300

    Update server.R

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ server <- function(input, output, session) {
   updateDateRangeInput(
     session = getDefaultReactiveDomain(),
     "fecha",
-    start = Sys.Date() - months(1),
+    start = Sys.Date() - months(1) + days(1),
     end   = Sys.Date(),
     max   = Sys.Date()
     )
@@ -27,8 +27,27 @@ server <- function(input, output, session) {
   })
   
   # inicialización
-  data_noticias <- reactiveVal(get_noticias_date_range(Sys.Date() - months(1), Sys.Date()))
-  # data_noticias <- get_noticias_date_range(Sys.Date() - months(1), Sys.Date())
+  data_noticias <- reactiveVal(get_noticias_date_range(Sys.Date() - months(1) + days(1), Sys.Date()))
+  # data_noticias <- get_noticias_date_range(Sys.Date() - months(1) + days(1), Sys.Date())
+  
+  output$hc_noticiasc <- renderHighchart({
+    data_noticias <- isolate(data_noticias())
+    
+    data_noticias_categorias <- get_noticias_categorias(data_noticias)
+    
+    highchart() |>
+      # hc_subtitle(text = "Treding Temas") |> 
+      hc_add_series(
+        data = data_noticias_categorias$n,
+        id = "data",
+        type = "bar",
+        color = PARS$color_chart,
+        showInLegend = FALSE,
+        name = "Noticias por categoría"
+      ) |> 
+      hc_xAxis(categories = data_noticias_categorias$categoria)
+    
+  })
   
   output$hc_conceptos <- renderHighchart({
     data_noticias <- isolate(data_noticias())
@@ -50,22 +69,6 @@ server <- function(input, output, session) {
           point = list(events = list(click = JS("function(){ Shiny.onInputChange('term', this.category) }")))
         )
       )
-  })
-  
-  output$hc_noticiasc <- renderHighchart({
-    data_noticias <- isolate(data_noticias())
-    data_noticias_categorias <- get_noticias_categorias(data_noticias)
-    highchart() |>
-      # hc_subtitle(text = "Treding Temas") |> 
-      hc_add_series(
-        data = data_noticias_categorias$n,
-        id = "data",
-        type = "bar",
-        color = PARS$color_chart,
-        showInLegend = FALSE,
-        name = "Noticias por categoría"
-      ) |> 
-      hc_xAxis(categories = data_noticias_categorias$categoria)
   })
   
   output$hc_gorepresc <- renderHighchart({
@@ -95,7 +98,8 @@ server <- function(input, output, session) {
         ),
         marker = list(enabled = TRUE),
         maxSize = "100%",
-        minSize = "5%"
+        minSize = "5%",
+        states = list(hover = list(enabled = FALSE))
       ) |> 
       hc_tooltip(
         valueDecimals = 2,
@@ -135,6 +139,20 @@ server <- function(input, output, session) {
 
   })
   
+  # mapa principal
+  output$map <- renderLeaflet({
+    
+    leaflet(
+      options = leafletOptions(
+        attributionControl = FALSE,
+        zoomControl = TRUE
+      )
+    ) |>
+      setView(lng =  -70.64827, lat = -33.45694, zoom = 9) |>
+      addProviderTiles(providers$CartoDB.Positron,  group = "Administrativo") 
+    
+  })
+  
   # data_noticias <- reactive({
   #   data_noticias <- get_noticias_date_range(input$fecha[1], input$fecha[2], input$categorias)
   #   }) |>
@@ -153,7 +171,7 @@ server <- function(input, output, session) {
     # data_noticias            <- get_noticias_date_range(input$fecha[1], input$fecha[2], input$categorias, input$comunas)
     data_noticias(get_noticias_date_range(input$fecha[1], input$fecha[2], input$categorias, input$comunas))
     
-    data_noticias            <- isolate(data_noticias())
+    data_noticias            <- data_noticias()
     data_noticias_ngram      <- get_noticias_ngram(data_noticias, as.numeric(input$ng))
     data_noticias_categorias <- get_noticias_categorias(data_noticias)
     
@@ -176,15 +194,15 @@ server <- function(input, output, session) {
       ) |>
       complete(date, sentiment, fill = list(n = 0))
 
-    glimpse(data_noticias_prescgore)
-    glimpse(data_noticias_gorepresc)
+    # glimpse(data_noticias_prescgore)
+    # glimpse(data_noticias_gorepresc)
   
     cli::cli_alert_info("observe actualización graficos: graficos")
     
     highchartProxy("hc_conceptos") |> 
       hcpxy_update(xAxis = list(categories = head(data_noticias_ngram$ngram, 10))) |> 
       hcpxy_update_series(id = "data", data =  head(data_noticias_ngram$n, 10))
-    
+
     highchartProxy("hc_noticiasc") |> 
       hcpxy_update(xAxis = list(categories = data_noticias_categorias$categoria)) |> 
       hcpxy_update_series(id = "data", data =  data_noticias_categorias$n)
@@ -206,10 +224,83 @@ server <- function(input, output, session) {
     highchartProxy("hc_prcepcion") |> hcpxy_loading(action = "hide")
     
     cli::cli_alert_info("observe actualización graficos: fin")
+    
+    
+    
   # })
   }) |>
-    bindEvent(input$fecha, input$categorias, input$comunas) |>
-    debounce(3000)
+    bindEvent(input$fecha, input$categorias, input$comunas)
+  
+  # mismo event que el de los graficos, pero para separar las logicas
+  observe({
+    
+    data_noticias            <- data_noticias()
+    
+    dgeo <- data_noticias |>
+      count(categoria, comunas) |> 
+      mutate(comunas = map(comunas, ~ str_squish(unlist(str_split(.x, "\\,"))))) |> 
+      unnest(comunas) |>
+      mutate(comunas = str_remove_all(comunas, "\"|\\'|\\[|\\]")) |> 
+      filter(comunas != "") |> 
+      count(comuna = comunas, wt = n, sort = TRUE)
+    
+    dgeo <- left_join(dcomunas, dgeo, by = join_by(comuna)) |> 
+      mutate(
+        # n = coalesce(n, 0),
+        id = snakecase::to_snake_case(stringi::stri_trans_general(comuna, id = "Latin-ASCII")),
+        nf = coalesce(comma(n), "sin")
+      )
+    
+    # viridisLite::plasma(15, end = .85) |> scales::show_col()
+    cols <-  viridisLite::plasma(15, end = .85, direction = -1)
+    # cols <- sort(PARS$palette)
+    colorData <- dgeo[["n"]]
+    pal  <- colorBin(cols, colorData, 7, pretty = TRUE, na.color = "#C0C0C0")
+    
+    # m <- leaflet() |> addTiles()
+    m <- leafletProxy("map")
+    
+    m |> 
+      clearShapes() |>
+      clearTopoJSON() |>
+      leaflet::addPolygons(
+        data             = dgeo,
+        fillColor        = ~ pal(colorData),
+        weight           = .5,
+        dashArray        = "3",
+        stroke           = NULL,
+        fillOpacity      = 0.7,
+        layerId          = ~ id,
+        # popup            = popp,
+        label            =  ~ str_glue("{comuna}: {nf} noticias."),
+        highlightOptions = highlightOptions(
+          color        = "white",
+          weight       = 2,
+          fillColor    = PARS$color_chart,
+          bringToFront = TRUE
+        ),
+        labelOptions = labelOptions(
+          style = list(
+            "font-family"  = PARS$base_font,
+            "box-shadow"   = "2px 2px rgba(0,0,0,0.15)",
+            "font-size"    = "15px",
+            "padding"      = "15px",
+            "border-color" = "rgba(0,0,0,0.15)"
+          )
+        )
+      ) |> 
+      addLegend(
+        position  = "topright",
+        na.label  = "Sin noticias",
+        pal       = pal,
+        values    = colorData,
+        # labFormat = labelFormat(transform = function(x) sort(x, decreasing = FALSE)),
+        layerId   = "colorLegend",
+        title = "Nº noticias"
+      )
+
+  }) |>
+    bindEvent(input$fecha, input$categorias, input$comunas, input$mainnav) 
 
   observe({
 
